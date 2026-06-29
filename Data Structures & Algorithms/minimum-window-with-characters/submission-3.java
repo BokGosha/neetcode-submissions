c225b1a552e341c7c20290e7c032dac6be254ed6
George Bokoev <147842599+BokGosha@users.noreply.github.com>
2026-06-29 22:20:21 +0300

    Add: minimum-window-with-characters - submission-3

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-3.java
@@ -0,0 +1,43 @@
+class Solution {
+    public static String minWindow(String s, String t) {
+        if (t.isEmpty()) {
+            return "";
+        }
+
+        Map<Character, Integer> countT = new HashMap<>();
+        Map<Character, Integer> window = new HashMap<>();
+
+        for (Character ch : t.toCharArray()) {
+            countT.put(ch, countT.getOrDefault(ch, 0) + 1);
+        }
+
+        int have = 0;
+        int need = countT.size();
+        int left = 0;
+        int resLen = Integer.MAX_VALUE;
+        String line = "";
+        for (int right = 0; right < s.length(); right++) {
+            Character c = s.charAt(right);
+            window.put(c, window.getOrDefault(c, 0) + 1);
+            if (countT.containsKey(c) && Objects.equals(window.get(c), countT.get(c))) {
+                have++;
+            }
+            
+            while (have == need) {
+                while ((right - left + 1) < resLen) {
+                    line = s.substring(left, right + 1);
+                    resLen = right - left + 1;
+                }
+
+                window.put(s.charAt(left), window.get(s.charAt(left)) - 1);
+                if (countT.containsKey(s.charAt(left)) && window.get(s.charAt(left)) < countT.get(s.charAt(left))) {
+                    have--;
+                }
+
+                left++;
+            }
+        }
+
+        return line;
+    }
+}
