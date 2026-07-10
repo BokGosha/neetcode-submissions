28f80be97b729879e4cc7a0ba9886535632f195d
George Bokoev <147842599+BokGosha@users.noreply.github.com>
2026-07-10 15:09:29 +0300

    Add: jump-game - submission-4

---
+++ Data Structures & Algorithms/jump-game/submission-4.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static boolean canJump(int[] nums) {
+        int goal = nums.length - 1;
+        for (int i = nums.length - 1; i > -1; i--) {
+            if (i + nums[i] >= goal) {
+                goal = i;
+            }
+        }
+
+        if (goal == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
